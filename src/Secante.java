483bed33cb27f166f834abdadf93b8b0667c1095
Camotubi <camotubi@gmail.com>
2017-06-11 15:31:45 -0500

    add metodo secante

--- src/Secante.java
+++ src/Secante.java
@@ -1,4 +1,30 @@
 
 public class Secante {
+	
+	
+	public static Double solve (PolinomialFunc f, double x0, double x1) throws Exception
+	{
+		double xn;
+		int n=1,nmax=1000 ;
+		double error=0.01;
+		boolean stop=false;
+		do
+		{
+			xn= x1-(f.evaluate(x1)*(x0-x1)/(f.evaluate(x0)-f.evaluate(x1)));
+			if(Math.abs(xn-x0)/Math.abs(xn)<=error)
+			{
+				stop=true;
+			}
+			else
+			{
+				if(n>nmax)
+				{
+					throw new Exception("Se alcanzo el maximo de iteraciones.");
+				}
+			}
+		}while(!stop);
+		
+		return xn;
+	}
 
 }
